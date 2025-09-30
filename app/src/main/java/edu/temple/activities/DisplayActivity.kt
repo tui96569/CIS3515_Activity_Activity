016be3341da8b0ca99968a065db5425a635669c7
tui96569 <tui96569@temple.edu>
2025-09-30 15:05:05 -0400

    activity finish

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -11,7 +11,8 @@ class DisplayActivity : AppCompatActivity() {
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            val textSize = intent.getIntExtra("TEXT_SIZE", 20)
+            this.textSize = textSize.toFloat()
         }
 
 
